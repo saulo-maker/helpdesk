ede8b977ef3de173207967d00d639699aa63824b
“Saulo <saulosousasilva3@gmail.com>
2023-04-02 20:29:02 -0300

    Criando metodos classe service de chamado

--- src/main/java/com/saulodev/resource/ChamadoResource.java
+++ src/main/java/com/saulodev/resource/ChamadoResource.java
@@ -1,5 +1,10 @@
 package com.saulodev.resource;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/chamado")
 public class ChamadoResource {
 	
 
--- src/main/java/com/saulodev/service/ChamadoService.java
+++ src/main/java/com/saulodev/service/ChamadoService.java
@@ -1,5 +1,28 @@
 package com.saulodev.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.saulodev.domain.Chamado;
+import com.saulodev.repository.ChamadoRepository;
+
+@Service
 public class ChamadoService {
     
+    @Autowired
+    private ChamadoRepository repository;
+
+    public Chamado findByid(Integer id) {
+        return repository.findById(id).orElseThrow();
+    }
+
+    public List<Chamado> findAll() {
+        return repository.findAll();
+    }
+
+    public Chamado createChamado(Chamado chamado) {
+        return repository.save(chamado);
+    }
 }
